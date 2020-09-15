64802de07607041b0c844361da082aea723accf0
Sarvesh Singh <sarvesh.singh@daffodilsw.com>
2020-09-15 21:35:11 +0530

    Update

--- src/server/index.js
+++ src/server/index.js
@@ -5,11 +5,13 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, '..', '..', 'dist')));
 
-app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, '..', '..', 'dist', 'index.html'));
 });
 
-app.listen(process.env.PORT || 8090);
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Running @ ${process.env.PORT || 3000}`);
+});
 
 //import React from 'react';
 //import { createStore } from 'redux';
